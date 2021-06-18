ebb943ffdf272b08eb50e6263d61206eb8dd3a95
nimacodes <32491455+nimacodes@users.noreply.github.com>
2021-06-18 08:17:26 +0200

    Signed for use in MoneyMoney App

--- hfcfra.lua
+++ hfcfra.lua
@@ -37,13 +37,13 @@ local _username
 
 
 function InitializeSession (protocol, bankCode, username, customer, password)
-  
+
     -- Fetch login page.
     connection.language = "de-de"
     html = HTML(connection:get("https://www.hfc-frankfurt.de/index.php?op=login"))
-	
-	_username = username
-	
+
+        _username = username
+
     html:xpath("//input[@name='userid']"):attr("value", username)
     html:xpath("//input[@name='password']"):attr("value", password)
 
@@ -70,48 +70,48 @@ end
 
 function RefreshAccount (account, since)
   -- Return balance and array of transactions.
-  local _transactions = {}  
-  
-  html = HTML(connection:get("https://www.hfc-frankfurt.de/account.php"))  
+  local _transactions = {}
+
+  html = HTML(connection:get("https://www.hfc-frankfurt.de/account.php"))
   local _balanceTable = html:xpath("//table[@class='manager rwd']")
-  
+
   -- Parse each transaction
   local _transactionsRows = _balanceTable:xpath("(//table//tbody//tr)")
-  
-  for i=1,_transactionsRows:length() do 	  
-	  local _currentRow = _transactionsRows:get(i)
-	  
-	  local _leadElement = _currentRow:children():get(1):text()
-	  
-	  
-	   if (_leadElement == 'V' or _leadElement == 'U' or _leadElement == 'L' or _leadElement == 'N' or _leadElement == 'J') then
-		 
-	 	 _transactions[#_transactions+1] = {
 
-		     bookingDate = getPOSIXdate(_currentRow:children():get(2):text()),
-			 			 
-		     purpose = getTransactionEntry(_currentRow:children():get(1):text(),
-			 							   _currentRow:children():get(2):text(),
-		 								   _currentRow:children():get(3):text(),
-									   	   _currentRow:children():get(4):text(),
-										   _currentRow:children():get(5):text(),
-										   _currentRow:children():get(6):text(),
-										   _currentRow:children():get(7):text(),
-										   _currentRow:children():get(8):text(),
-										  _currentRow:children():get(9):text(),
-										  _currentRow:children():get(10):text()),
-		     amount = toNumber(_currentRow:children():get(11):text())
-	 	 }
-		 
-	   end
+  for i=1,_transactionsRows:length() do
+          local _currentRow = _transactionsRows:get(i)
+
+          local _leadElement = _currentRow:children():get(1):text()
+
+
+           if (_leadElement == 'V' or _leadElement == 'U' or _leadElement == 'L' or _leadElement == 'N' or _leadElement == 'J') then
+
+                  _transactions[#_transactions+1] = {
+
+                     bookingDate = getPOSIXdate(_currentRow:children():get(2):text()),
+
+                     purpose = getTransactionEntry(_currentRow:children():get(1):text(),
+                                                                                    _currentRow:children():get(2):text(),
+                                                                                    _currentRow:children():get(3):text(),
+                                                                                      _currentRow:children():get(4):text(),
+                                                                                   _currentRow:children():get(5):text(),
+                                                                                   _currentRow:children():get(6):text(),
+                                                                                   _currentRow:children():get(7):text(),
+                                                                                   _currentRow:children():get(8):text(),
+                                                                                  _currentRow:children():get(9):text(),
+                                                                                  _currentRow:children():get(10):text()),
+                     amount = toNumber(_currentRow:children():get(11):text())
+                  }
+
+           end
   end
   -- End transaction
-  
-  
+
+
   -- Begin balance
-  local _balance = _balanceTable:xpath("(//table//td[@data-header='Kontostand'])"):text() 
+  local _balance = _balanceTable:xpath("(//table//td[@data-header='Kontostand'])"):text()
   -- End balance
-     
+
   return {balance=toNumber(_balance), transactions=_transactions}
 end
 
@@ -121,44 +121,44 @@ end
 
 -- Helper
 function getTransactionEntry(type, date, acreg, from, to, flighttime, flightCosts, landings, landingsCost, comments)
-	local _workstring
-	
-	if (type== 'V') then
-		_workstring = "Vortrag\n" .. "Comments: " .. comments
-	elseif (type == 'U') then
-		_workstring = "Überweisung\n" .. "Comments: " .. comments
-	elseif (type == 'L') then
-		_workstring = "Schulungsflug\n" ..  "AC: " .. string.upper(acreg) .. " " ..
-					"From: " .. string.upper(from) .. " " ..
-					"To: " .. string.upper(to) .. " " ..
-					"Date: " .. date .. " " ..
-					"Flight Time: " .. flighttime .. " " ..
-					"Flight Cost: " .. flightCosts .. " " ..
-					"Landings: " .. landings .. " " ..
-					"Landings Cost: " .. landingsCost .. " " 
-	elseif (type == 'J') then
-		_workstring = "Jahresbeitrag\n" .. "Comments: " .. comments
-	elseif (type == 'N') then
-		_workstring = "Flug\n" .. "AC: " .. string.upper(acreg) .. " " ..
-					"From: " .. string.upper(from) .. " " ..
-					"To: " .. string.upper(to) .. " " ..
-					"Date: " .. date .. " " ..
-					"Flight Time: " .. flighttime .. " " ..
-					"Flight Cost: " .. flightCosts .. " " ..
-					"Landings: " .. landings .. " " ..
-					"Landings Cost: " .. landingsCost .. " " 
-	else
-		_workstring = "Sonstiges\n" .. "Comments: " .. comments
-	end
-	
-	return _workstring	
+        local _workstring
+
+        if (type== 'V') then
+                _workstring = "Vortrag\n" .. "Comments: " .. comments
+        elseif (type == 'U') then
+                _workstring = "Überweisung\n" .. "Comments: " .. comments
+        elseif (type == 'L') then
+                _workstring = "Schulungsflug\n" ..  "AC: " .. string.upper(acreg) .. " " ..
+                                        "From: " .. string.upper(from) .. " " ..
+                                        "To: " .. string.upper(to) .. " " ..
+                                        "Date: " .. date .. " " ..
+                                        "Flight Time: " .. flighttime .. " " ..
+                                        "Flight Cost: " .. flightCosts .. " " ..
+                                        "Landings: " .. landings .. " " ..
+                                        "Landings Cost: " .. landingsCost .. " "
+        elseif (type == 'J') then
+                _workstring = "Jahresbeitrag\n" .. "Comments: " .. comments
+        elseif (type == 'N') then
+                _workstring = "Flug\n" .. "AC: " .. string.upper(acreg) .. " " ..
+                                        "From: " .. string.upper(from) .. " " ..
+                                        "To: " .. string.upper(to) .. " " ..
+                                        "Date: " .. date .. " " ..
+                                        "Flight Time: " .. flighttime .. " " ..
+                                        "Flight Cost: " .. flightCosts .. " " ..
+                                        "Landings: " .. landings .. " " ..
+                                        "Landings Cost: " .. landingsCost .. " "
+        else
+                _workstring = "Sonstiges\n" .. "Comments: " .. comments
+        end
+
+        return _workstring
 end
 
 function getPOSIXdate(str)
-	local _splits = mysplit(str,"-")
-	
+        local _splits = mysplit(str,"-")
 
-	return os.time{day=_splits[3], year=_splits[1], month=_splits[2]}
+
+        return os.time{day=_splits[3], year=_splits[1], month=_splits[2]}
 end
 
 function mysplit (inputstr, sep)
@@ -174,22 +174,22 @@ end
 
 function toNumber(str)
 
-	local _workstring = trim1(str)	
-	local _mult = 1
-	
-	_workstring = string.sub(_workstring,0,string.len(_workstring))
-	
-	if string.match(_workstring, "-") then
-		_mult = -1
-	end 
-	
+        local _workstring = trim1(str)
+        local _mult = 1
 
-	_workstring = string.match(_workstring,'%f[%d]%d[,.%d]*%f[%D]')
-	
-	_workstring = string.gsub(_workstring,",",".")
-		
-	local _number = _workstring * _mult
-	return _number
+        _workstring = string.sub(_workstring,0,string.len(_workstring))
+
+        if string.match(_workstring, "-") then
+                _mult = -1
+        end
+
+
+        _workstring = string.match(_workstring,'%f[%d]%d[,.%d]*%f[%D]')
+
+        _workstring = string.gsub(_workstring,",",".")
+
+        local _number = _workstring * _mult
+        return _number
 end
 
 function trim1(s)
@@ -200,12 +200,12 @@ end
 -- modified fixed a serialization issue with invalid name. and wrap with 2 functions to serialize / deserialize
 
 function tableToString(table)
-	return "return"..serializeTable(table)
+        return "return"..serializeTable(table)
 end
 
 function stringToTable(str)
-	local f = loadstring(str)
-	return f()
+        local f = loadstring(str)
+        return f()
 end
 
 function serializeTable(val, name, skipnewlines, depth)
@@ -213,12 +213,12 @@ function serializeTable(val, name, skipnewlines, depth)
     depth = depth or 0
 
     local tmp = string.rep(" ", depth)
-    if name then 
-    	if not string.match(name, '^[a-zA-z_][a-zA-Z0-9_]*$') then
-    		name = string.gsub(name, "'", "\\'")
-    		name = "['".. name .. "']"
-    	end
-    	tmp = tmp .. name .. " = "
+    if name then
+            if not string.match(name, '^[a-zA-z_][a-zA-Z0-9_]*$') then
+                    name = string.gsub(name, "'", "\\'")
+                    name = "['".. name .. "']"
+            end
+            tmp = tmp .. name .. " = "
      end
 
     if type(val) == "table" then
@@ -241,3 +241,5 @@ function serializeTable(val, name, skipnewlines, depth)
 
     return tmp
 end
+
+-- SIGNATURE: MCwCFEs1QAdLuu6WYxsVnnVRkYCOqyKoAhQKoEnYY2FuPWKAlMC3AOPyYX4RXA==
